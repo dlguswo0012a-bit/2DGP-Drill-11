28346ffb6a1c5ba2203828e7e0f48b6a0b7ca966
lee hyeonjae <dlguswo0012a@gmail.com>
2025-11-04 11:58:09 +0900

    start

---
+++ ball.py
@@ -0,0 +1,41 @@
+from pico2d import *
+import game_world
+import game_framework
+
+PIXEL_PER_METER = (1.0 / 0.03)  # 1pixel = 3cm, 1m = 33.33 pixel
+GRAVITY = 9.8  # 중력 가속도 (m/s²)
+
+class Ball:
+    image = None
+
+    def __init__(self, x = 400, y = 300, throwin_speed = 15, throwin_angle = 45):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y = x, y
+        self.xv = throwin_speed * math.cos(math.radians(throwin_angle))  # m/s
+        self.yv = abs(throwin_speed * math.sin(math.radians(throwin_angle)))   # m/s
+        self.stopped = True if throwin_speed == 0.0 else False
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        #draw_rectangle의 경우 4개의 파라미터 필요, get_bb()의 경우 값이 튜플임 -> 언팩킹 필요(*사용
+        draw_rectangle(*self.get_bb())
+
+    def update(self):
+        if self.stopped:
+            return
+        # 위치 업데이트
+        self.x += self.xv * game_framework.frame_time * PIXEL_PER_METER
+        self.y += self.yv * game_framework.frame_time * PIXEL_PER_METER
+
+        # y 축 속도에 중력 가속도 적용
+        self.yv -= GRAVITY * game_framework.frame_time  # m/s
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def handle_collision(self, group, other):
+        if group == 'boy:ball':
+            game_world.remove_object(self)
+        elif group == 'grass:ball':
+            self.stopped = True
---
+++ play_mode.py
@@ -0,0 +1,72 @@
+import random
+from pico2d import *
+
+import game_framework
+import game_world
+
+from boy import Boy
+from grass import Grass
+from ball import Ball
+from zombie import Zombie
+
+boy = None
+
+def handle_events():
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+def init():
+    global boy
+    global balls
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+    game_world.add_collision_pair('grass:ball', grass, None) #[[grass], []]
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+    balls = [Ball(random.randint(100, 1600-100),60,0)for _ in range(30)]
+    game_world.add_objects(balls, 1)
+
+    zombies = [Zombie() for _ in range(4)]
+    game_world.add_objects(zombies,1)
+
+    #소년과 공 사이에 충돌검사가 필요하다는 정보를 추가
+    game_world.add_collision_pair('boy:ball', boy, None) #[[boy], []]
+    for ball in balls:
+        game_world.add_collision_pair('boy:ball',None,ball)#[[boy], [ball1, ball2, ...]]
+
+
+def update():
+    game_world.update()
+    #충돌 처리할 객체들이 적으면 상관 없지만 많다면? => 충돌그룹을 만들어서 처리 ex) boy:ball, grass:ball
+    #for ball in balls.copy():
+        #if game_world.collide(boy, ball):
+         #   print('COLLISION boy: ball')
+         #   boy.ball_count += 1
+         #   game_world.remove_object(ball)
+            #게임월드에서는 지워졌지만 balls 리스트에서는 남아있으므로 제거
+         #   balls.remove(ball)
+    game_world.handle_collisions()
+
+
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+
+def finish():
+    game_world.clear()
+
+def pause(): pass
+def resume(): pass
+
